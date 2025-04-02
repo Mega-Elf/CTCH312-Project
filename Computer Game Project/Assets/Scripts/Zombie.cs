6fb55d0bac8d36cb683320b44dff10c58816914a
Alexander Baht <awebaht@gmail.com>
2025-04-01 20:17:13 -0600

    Added zombie model and animations
    added zombie asset from unity store with animations and animation for hit reaction from mixamo. Zombie moves and reacts when shot, and plays one of two random death animations.

--- Computer Game Project/Assets/Scripts/Zombie.cs
+++ Computer Game Project/Assets/Scripts/Zombie.cs
@@ -1,14 +1,34 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Zombie : MonoBehaviour
 {
     [SerializeField] private int health = 50;
     private Animator animator;
 
+    private NavMeshAgent navAgent;
+
+    private CapsuleCollider hitBox;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
+        navAgent = GetComponent<NavMeshAgent>();
+        hitBox = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navAgent.velocity.magnitude > 0.1f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
     }
 
     public void TakeDamage(int damageTaken)
@@ -17,8 +37,19 @@ public class Zombie : MonoBehaviour
 
         if (health <= 0)
         {
-            animator.SetTrigger("DEATH");
-            Destroy(gameObject);
+            hitBox.enabled = false; // turn off hitbox when zombie dies
+
+            int randomNumber = Random.Range(0, 2); // random number 0 or 1, which death effect plays
+            if (randomNumber == 0)
+            {
+                animator.SetTrigger("DEATH_1");
+            }
+            else
+            {
+                animator.SetTrigger("DEATH_2");
+            }
+
+            // Destroy(gameObject);
         }
         else
         {
